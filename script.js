3e2c5edcf99931adfde9c8c2a6d994889b14f416
Raza Hanif <raza_hanif@ymail.com>
2025-03-17 21:10:32 -0400

    disable gradient on mobile

--- script.js
+++ script.js
@@ -2,17 +2,19 @@ const main = document.querySelector('.main')
 const cursor = document.querySelector('.cursor')
 
 if (main && cursor) {
-    main.addEventListener('mousemove', (e) => {
-        const x = e.clientX
-        const y = e.clientY
-        cursor.style.background = `radial-gradient(circle 300px at ${x}px ${y}px, #031E35, #010B13)`
-    })
-
-    main.addEventListener('mouseenter', () => {
-        cursor.style.opacity = 1
-    })
-
-    main.addEventListener('mouseleave', () => {
-        cursor.style.opacity = 0
-    })
+    if (!window.matchMedia('(any-pointer: coarse)').matches) {
+        main.addEventListener('mousemove', (e) => {
+            const x = e.clientX
+            const y = e.clientY
+            cursor.style.background = `radial-gradient(circle 300px at ${x}px ${y}px, #031E35, #010B13)`
+        })
+    
+        main.addEventListener('mouseenter', () => {
+            cursor.style.opacity = 1
+        })
+    
+        main.addEventListener('mouseleave', () => {
+            cursor.style.opacity = 0
+        })
+    }
 }

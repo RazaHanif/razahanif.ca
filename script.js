3efc84717d710a506677a80fcc51817c7cd8aa9f
Raza Hanif <raza_hanif@ymail.com>
2025-03-17 20:51:51 -0400

    trying to add some dynamic css

---
+++ script.js
@@ -0,0 +1,19 @@
+const main = document.querySelector('.main')
+const cursor = document.querySelector('.cursor')
+
+if (main && cursor) {
+    main.addEventListener('mousemove', (e) => {
+        const x = e.clientX
+        const y = e.clientY
+        cursor.style.background = `radial-gradient(circle 300px at ${x}px ${y}px, #031E35, #010B13)`
+    })
+
+    main.addEventListener('mouseenter', () => {
+        cursor.style.opacity = 1
+    })
+
+    main.addEventListener('mouseleave', () => {
+        cursor.style.opacity = 0
+    })
+
+}
